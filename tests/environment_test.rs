b4e75bea7fda01183f648f7105cfc4cb42a4322d
Kevin Lacker <lacker@gmail.com>
2024-10-23 14:18:05 -0700

    no repeating cases

--- tests/environment_test.rs
+++ tests/environment_test.rs
@@ -1349,4 +1349,33 @@ theorem add_assoc(a: Nat, b: Nat, c: Nat) { add(add(a, b), c) = add(a, add(b, c)
             "#,
         );
     }
+
+    #[test]
+    fn test_match_no_repeating_cases() {
+        let mut env = Environment::new_test();
+        env.add(
+            r#"
+            inductive Foo {
+                bar(Bool)
+                baz
+            }"#,
+        );
+        env.bad(
+            r#"
+            define foo(f: Foo) -> Bool {
+                match f {
+                    Foo.bar(b) {
+                        b
+                    }
+                    Foo.baz {
+                        false
+                    }
+                    Foo.bar(b) {
+                        b
+                    }
+                }
+            }
+        "#,
+        );
+    }
 }
