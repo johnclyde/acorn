451b364ef58a01f1c88789b2b1a6e3b69d6295c5
Kevin Lacker <lacker@gmail.com>
2025-03-24 10:46:46 -0700

    tdd red

--- tests/environment_test.rs
+++ tests/environment_test.rs
@@ -2551,4 +2551,33 @@ theorem add_assoc(a: Nat, b: Nat, c: Nat) { add(add(a, b), c) = add(a, add(b, c)
             "#,
         );
     }
+
+    #[test]
+    fn test_env_instance_statement_no_extra_attributes() {
+        let mut env = Environment::new_test();
+        env.add(
+            r#"
+            typeclass M: Magma {
+                mul: (M, M) -> M
+            }
+
+            inductive State {
+                clean
+                dirty
+            }
+            "#,
+        );
+        env.bad(
+            r#"
+            instance State: Magma {
+                define mul(self, other: State) -> State {
+                    State.clean
+                }
+                define add(self, other: State) -> State {
+                    State.clean
+                }
+            }
+            "#,
+        );
+    }
 }
