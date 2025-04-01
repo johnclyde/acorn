1430dc2d543431fbb8d66c1c894660863b9e84b7
Kevin Lacker <lacker@gmail.com>
2025-04-01 16:45:20 -0700

    put depth on the source

--- src/monomorphizer.rs
+++ src/monomorphizer.rs
@@ -162,6 +162,12 @@ impl Monomorphizer {
     pub fn add_fact(&mut self, fact: Fact) {
         match fact {
             Fact::Proposition(proposition, truthiness) => {
+                if truthiness == Truthiness::Factual {
+                    assert_eq!(proposition.source.depth, 0);
+                }
+                if truthiness == Truthiness::Hypothetical {
+                    assert_ne!(proposition.source.depth, 0);
+                }
                 self.add_proposition(proposition, truthiness);
             }
             Fact::InstanceOf(class, typeclass) => {
