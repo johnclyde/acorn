47e37a367999072b8d2436ba6bcf087e06cb3529
Kevin Lacker <lacker@gmail.com>
2025-04-21 11:19:41 -0700

    tdd red

--- tests/prover_test.rs
+++ tests/prover_test.rs
@@ -2128,4 +2128,32 @@ mod prover_test {
         "#;
         verify_succeeds(text);
     }
+
+    // #[test]
+    // fn test_proving_with_parametrized_inductive() {
+    //     let text = r#"
+    //         inductive List<T> {
+    //             nil
+    //             cons(T, List<T>)
+    //         }
+
+    //         define any(bs: List<Bool>) -> Bool {
+    //             match bs {
+    //                 List.nil {
+    //                     false
+    //                 }
+    //                 List.cons(b, bs) {
+    //                     b or any(bs)
+    //                 }
+    //             }
+    //         }
+
+    //         theorem goal {
+    //             exists(bs: List<Bool>) {
+    //                 any(bs)
+    //             }
+    //         }
+    //     "#;
+    //     verify_succeeds(text);
+    // }
 }
