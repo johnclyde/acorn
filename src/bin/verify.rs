5057e3016ad3a19aa9987912863055596952e814
Kevin Lacker <lacker@gmail.com>
2025-05-08 12:39:26 -0700

    verifier

--- src/bin/verify.rs
+++ src/bin/verify.rs
@@ -8,9 +8,7 @@
 // Try:
 //   cargo build --release --bin=verify; time ~/acorn/target/release/verify
 
-use std::path::PathBuf;
-
-use acorn::project::Project;
+use acorn::verifier::{Verifier, VerifierMode};
 use clap::Parser;
 
 #[derive(Parser)]
@@ -38,64 +36,17 @@ struct Args {
 async fn main() {
     let args = Args::parse();
 
-    if args.full && args.filtered {
-        println!("--full and --filtered are incompatible.");
-        return;
-    }
-    let use_cache = !args.full;
-
-    let mut project = Project::new_local(use_cache).unwrap();
-    if args.filtered {
-        project.check_hashes = false;
-    }
-
-    if let Some(target) = args.target {
-        if target.ends_with(".ac") {
-            // Looks like a filename
-            let path = PathBuf::from(&target);
-            if !project.add_target_by_path(&path) {
-                println!("File not found: {}", target);
-                return;
-            }
-        } else {
-            if !project.add_target_by_name(&target) {
-                println!("Module not found: {}", target);
-                return;
-            }
+    let mode = if args.full {
+        if args.filtered {
+            println!("--full and --filtered are incompatible.");
+            return;
         }
+        VerifierMode::Full
+    } else if args.filtered {
+        VerifierMode::Filtered
     } else {
-        project.add_all_targets();
-    }
-
-    // Set up the builder
-    let mut builder = project.builder(|event| {
-        if let Some(m) = event.log_message {
-            if let Some(diagnostic) = event.diagnostic {
-                println!(
-                    "{}, line {}: {}",
-                    event.module,
-                    diagnostic.range.start.line + 1,
-                    m
-                );
-            } else {
-                println!("{}", m);
-            }
-        }
-    });
-    builder.log_when_slow = true;
-    if args.dataset {
-        builder.create_dataset();
-    }
-
-    // Build
-    project.build(&mut builder);
-    builder.print_stats();
-    if let Some(dataset) = builder.dataset {
-        dataset.save();
-    }
-
-    if args.filtered && builder.searches_full > 0 {
-        println!("\nWarning: the filtered prover was not able to handle all goals.");
-        std::process::exit(1);
-    }
+        VerifierMode::Standard
+    };
+    let verifier = Verifier::new(mode, args.target, args.dataset);
+    verifier.run();
 }
